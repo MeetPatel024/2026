f6518ae74ac1c2e31162ed16df647e73ad6bca65
Meet Patel <106991696+MeetPatel024@users.noreply.github.com>
2026-01-12 10:27:45 +0530

    Create Generate All Binary Strings.py

---
+++ python/CAD/Generate All Binary Strings.py
@@ -0,0 +1,43 @@
+def s(index,flag,numbers,result):
+    if index>=len(numbers):
+        result.append("".join(numbers))
+        return
+    numbers[index]="0"
+    s(index+1,True,numbers,result)
+    if flag==True:
+        numbers[index]="1"
+        s(index+1,False,numbers,result)
+        numbers[index]="0"
+
+N = 3
+numbers = ["0"]*N
+result = []
+s(0,True,numbers,result)
+print(result)
+
+
+
+
+
+"""
+class Solution:
+    
+    def s(self,index,flag,numbers,result):
+        if index>=len(numbers):
+            result.append("".join(numbers))
+            return
+        numbers[index]="0"  
+        self.s(index+1,True,numbers,result)
+        if flag==True:
+            numbers[index]="1"
+            self.s(index+1,False,numbers,result)
+            numbers[index]="0"
+            
+    
+    def binstr(self, n):
+        nums = ["0"]*n    
+        result = []
+        self.s(0,True,nums,result)
+
+        return result
+"""
